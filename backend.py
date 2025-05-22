11a41c8dc37e78c671f716bdd2a6330dbb692659
thanhdvu <thuyanhdv@gmail.com>
2025-05-22 13:27:54 +0900

    class

--- backend.py
+++ backend.py
@@ -1,2 +1,15 @@
 from datetime import date
 
+class civil_complaint:
+    def __init__(self, user, content, latitude, longtitude, created_date=None):
+        self.user = user
+        self.content = content
+        self.latitude = latitude
+        self.longtitude = longtitude
+        if created_date:
+            self.created_date = created_date
+        else:
+            self.created_date = date.today()
+
+    def __str__(self):
+        return f"[{self.created_date}] {self.user}님이 신고: {self.content} ({self.latitude}, {self.longtitude})"
