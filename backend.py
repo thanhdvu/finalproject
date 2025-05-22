140da4793686f2047c6b392d448cb429cc87930d
thanhdvu <thuyanhdv@gmail.com>
2025-05-22 21:50:32 +0900

    tested successfully

--- backend.py
+++ backend.py
@@ -2,18 +2,18 @@ from datetime import date
 from sheets_oauth import save_complaints_to_sheet
 
 class civil_complaint:
-    def __init__(self, user, content, latitude, longtitude, created_date=None):
+    def __init__(self, user, content, latitude, longitude, created_date=None):
         self.user = user
         self.content = content
         self.latitude = latitude
-        self.longtitude = longtitude
+        self.longitude = longitude
         if created_date:
             self.created_date = created_date
         else:
             self.created_date = date.today()
 
     def __str__(self):
-        return f"[{self.created_date}] {self.user}님이 신고: {self.content} ({self.latitude}, {self.longtitude})"
+        return f"[{self.created_date}] {self.user}님이 신고: {self.content} ({self.latitude}, {self.longitude})"
     
 def submit_complaint(user, content, latitude, longitude, created_date=None):
     complaint = civil_complaint(user, content, latitude, longitude, created_date)
