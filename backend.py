40b8bdb19b8bf52b8857d8ebaa4c944c172dda75
thanhdvu <thuyanhdv@gmail.com>
2025-05-22 21:48:04 +0900

    add submit_complaint

--- backend.py
+++ backend.py
@@ -1,4 +1,5 @@
 from datetime import date
+from sheets_oauth import save_complaints_to_sheet
 
 class civil_complaint:
     def __init__(self, user, content, latitude, longtitude, created_date=None):
@@ -13,3 +14,7 @@ class civil_complaint:
 
     def __str__(self):
         return f"[{self.created_date}] {self.user}님이 신고: {self.content} ({self.latitude}, {self.longtitude})"
+    
+def submit_complaint(user, content, latitude, longitude, created_date=None):
+    complaint = civil_complaint(user, content, latitude, longitude, created_date)
+    save_complaints_to_sheet(complaint)
