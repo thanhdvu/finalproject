3df5232aa27d59895213b34675a1ecd0e0ec631f
yeondubu05 <yeoneunseo2005@gmail.com>
2025-06-03 16:38:05 +0900

    페이지 이름들을 영어로 통일함

--- Home.py
+++ Home.py
@@ -12,12 +12,13 @@ st.session_state['lang'] = lang
 if lang == 'Korean':
     st.title("📌 민원 신고 플랫폼")
     st.markdown("""
-    ### 주민의 불편함을 손쉽게 신고하고, 함께 해결해요!
+    ### 주민의 불편함을 손쉽게 신고하고, 함께 해결해요! 
+    사이드바에서 언어를 먼저 선택하고, 
     아래 버튼을 눌러 민원 신고를 시작해보세요!
     """)
-    st.page_link("pages/1_👀민원_사용법_-_How_to_Use.py", label="👀 민원 사용법 - How to Use")
-    st.page_link("pages/2_❗️민원_신고하기_-_Report_a_Complaint.py", label="❗ 민원 신고하기 - Report a Complaint")
-    st.page_link("pages/3_🔍민원_검색하기_-_Search_Complaints.py", label="🔍 민원 검색하기 - Search Complaints")
+    st.page_link("pages/1_👀_How_To_Use.py", label="👀 민원 사용법")
+    st.page_link("pages/2_❗️_Submit_Complaint.py", label="❗ 민원 신고하기")
+    st.page_link("pages/3_🔍_Search_Complaints.py", label="🔍 민원 검색하기")
 
 else:
     st.title("📌 Complaint Reporting Platform")
@@ -25,6 +26,6 @@ else:
     ### Easily report residents' inconveniences and solve them together!
     Click the buttons below to start reporting!
     """)
-    st.page_link("pages/1_👀민원_사용법_-_How_to_Use.py", label="👀 민원 사용법 - How to Use")
-    st.page_link("pages/2_❗️민원_신고하기_-_Report_a_Complaint.py", label="❗ 민원 신고하기 - Report a Complaint")
-    st.page_link("pages/3_🔍민원_검색하기_-_Search_Complaints.py", label="🔍 민원 검색하기 - Search Complaints")
+    st.page_link("pages/1_👀_How_To_Use.py", label="👀 How to Use")
+    st.page_link("pages/2_❗️_Submit_Complaint.py", label="❗ Report Complaint")
+    st.page_link("pages/3_🔍_Search_Complaints.py", label="🔍 Search Complaints")
